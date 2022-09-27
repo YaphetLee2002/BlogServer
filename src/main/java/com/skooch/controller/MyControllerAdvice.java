dc304a5e84a8f995819cf69192cbde1dd039c174
skooch02 <lyy.skooch02@gmail.com>
2022-09-27 10:10:45 +0800

    22.09.27.10.10
    项目异常处理类改了个名字

--- src/main/java/com/skooch/controller/MyControllerAdvice.java
+++ src/main/java/com/skooch/controller/MyControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ProjectExceptionAdvice {
+public class MyControllerAdvice {
 
     @ExceptionHandler(SecurityException.class)
     public Result doSystemException(SystemException systemException) {
